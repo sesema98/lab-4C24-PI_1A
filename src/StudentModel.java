5a41c1b672398c5914a2b85ba72e46c3d4617e66
jhamile.macavilca@tecsup.edu.pe <victor.aucarure@example.com>
2025-08-28 15:33:25 -0500

    Update StudentModel.java: added age attribute

---
+++ src/StudentModel.java
@@ -0,0 +1,13 @@
+public class StudentModel {
+    private String name;
+    private int age;
+
+    public StudentModel(String name, int age){
+        this.name = name;
+        this.age = age;
+    }
+
+    public String getName(){ return name; }
+    public int getAge(){ return age; }
+}
+
